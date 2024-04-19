13fe13019ff4c763403722d8608c17d1aa79fdea
TeBlanco <gio.cerda@duocuc.cl>
2024-04-19 15:40:20 +0000

    radio 8

---
+++ R10.py
@@ -0,0 +1,13 @@
+import math
+
+def calcular_circ(radio):
+    pi = round(math.pi, 6)
+
+    circ = 2 * pi * radio
+    
+   return circ
+   radio  = 10
+
+  circ = calcular_circ(radio)
+
+print ("un radio de 10 cm es", radio, "cm" )
